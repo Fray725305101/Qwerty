7782a07ac04f4a0eb428994e6ca050d58cb8855c
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-08-04 19:49:09 +0300

    Commit

--- src/Qwerty/Qwerty.java
+++ src/Qwerty/Qwerty.java
@@ -8,6 +8,12 @@ public class Qwerty {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter char: ");
         String inputChar = scanner.nextLine();
-
+        int idxInputChar = qwerty.indexOf(inputChar);
+        if (!inputChar.equals("q")) {
+            System.out.println(qwerty.charAt(idxInputChar - 1));
+        }
+        else {
+            System.out.println(qwerty.charAt(qwerty.length()-1));
+        }
     }
 }
