a9448dad07aaace628a1c2b371e66fe7ba17b55e
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-08-04 19:33:09 +0300

    Commit

--- src/Qwerty/Qwerty.java
+++ src/Qwerty/Qwerty.java
@@ -1,4 +1,13 @@
 package Qwerty;
 
+import java.util.Scanner;
+
 public class Qwerty {
+    public static void main (String[] args) {
+        String qwerty = "qwertyuiopasdfghjklzxcvbnm";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter char: ");
+        String inputChar = scanner.nextLine();
+
+    }
 }
