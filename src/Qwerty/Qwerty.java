cbf70ac82af08961415441621a14bcdf683a8c01
Andrei Pakudin <andrey.pakudin@yandex.ru>
2025-08-04 20:59:18 +0300

    Commit

--- src/Qwerty/Qwerty.java
+++ src/Qwerty/Qwerty.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Qwerty {
     public static void main (String[] args) {
-        String qwerty = "qwertyuiopasdfghjklzxcvbnm";
+        String qwerty = "qwertyuiopasdfghjklzxcvbnm"; //Задаём строку по раскладке qwerty
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter char: ");
         String inputChar = scanner.nextLine();
-        int idxInputChar = qwerty.indexOf(inputChar);
-        if (!inputChar.equals("q")) {
-            System.out.println("Left char is " + qwerty.charAt(idxInputChar - 1));
+        int idxInputChar = qwerty.indexOf(inputChar); //Определяем индекс введённого символа в подстроке
+        if (!inputChar.equals("q")) { //Если это не первый символ (совсем правильно было бы использовать индекс=0, но мы же хотим пощупать equals)
+            System.out.println("Left char is " + qwerty.charAt(idxInputChar - 1)); //Выводим предыдущий символ
         }
-        else {
-            System.out.println("Left char is " + qwerty.charAt(qwerty.length()-1));
+        else {//Иначе
+            System.out.println("Left char is " + qwerty.charAt(qwerty.length()-1)); //Последний символ
         }
     }
 }
